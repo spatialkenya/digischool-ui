8c5167d2d3078342c9dea803bcc2d718e342780e
Erick Otenyo <otenyo.erick@gmail.com>
2017-09-02 12:15:37 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import {Jane} from 'jane-maps';
 
-import {mapboxGLOptions, searchConfig} from './map.config'
+import {mapboxGLOptions, searchConfig} from './map.config';
 
 import SchoolJaneLayer from './schools/SchoolLayer';
 
-import logo from './logo.svg';
 import 'jane-maps/dist/styles.css'
 import './App.css';
 
@@ -17,9 +16,10 @@ const App = () => {
 
   return (
     <MuiThemeProvider>
-        <Jane mapboxGLOptions={mapboxGLOptions} search searchConfig={searchConfig}>
-          <SchoolJaneLayer defaultSelected/>
-        </Jane>
+
+      <Jane mapboxGLOptions={mapboxGLOptions} search searchConfig={searchConfig}>
+        <SchoolJaneLayer defaultSelected/>
+      </Jane>
     </MuiThemeProvider>
   );
 };
