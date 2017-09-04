5728f8c1dfa7feed37dd6d404d7151ac30f6548f
Grace-Amondi <ecofluff990@gmail.com>
2017-09-04 10:56:06 +0300

    added about data

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import Topbar from '../topbar/Topbar';
+
+import '/home/geogirl/Downloads/digischool-ui-master/src/pages/about/About.css';
 
 export default class About extends React.Component{
   render(){
     return(
       <div>
         <Topbar subhead={"About"}/>
+        <div classname="heading">
+        <br></br>
+        <br></br>
+        <h1>About DigiSchool Kenya</h1>
+        <h3>DigiSchool Kenya</h3>
+        </div>
+        <div>
+        <p>This is a management information system that brings together all Digischool information and
+        visualizes it for better understanding and flow of the information on class one laptops. </p>
+        </div>
       </div>
     );
   }
